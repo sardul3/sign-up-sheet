907e26616542bb7eb6803fc433f5f527827ba506
Sagar Poudel <poudelsagar530@gmail.com>
2018-08-30 14:52:21 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/users');
+
+var User = mongoose.model("User", {name: String, email: String});
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser({urlencoded: true}));
+
+app.get('/', (req, res)=>{
+    res.render('homepage');
+});
+
+app.post('/send', (req, res)=>{
+    var name = req.body.fname + " " + req.body.lname;
+    var email = req.body.email;
+    
+    new User({name: name, email: email}).save().then(()=>{
+        console.log('new user has logged in!');
+    });
+    res.redirect('/');
+});
+
+app.listen(process.env.PORT, process.env.IP, ()=>{
+   console.log('Server is up!'); 
+});
